ad66c5523a01bc8c64a6a0cb275daf986be034d3
Abdalla Ibrahim <new@SandKIDmegasMBP.attlocal.net>
2020-04-30 22:00:55 -0400

    fixed

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,38 +2,35 @@ const Workout = require ("../models/workout");
 
 module.exports = (app) => {
     app.get("/api/wokrouts", (req, res) => {
-        db.Workout.find({})
-            .then(Workout => {
-                res.json(Workout)
-            .catch(err => {
-                res.json(err)
-            })
+        Workout.find({})
+            .then(workout => {
+                res.json(workout)
+        }).catch(err => {
+            res.json(err)
         })
     })
 
     app.get("api/workots/:id", (req, res) => {
-        db.Workout.findOne({_id: req.params.id})
-        .then(Workout => {
-            res.json(Workout)
-        })
-        .catch(err => {
+        Workout.findOne({_id: req.params.id})
+        .then(workout => {
+            res.json(workout)
+        }).catch(err => {
             res.json(err)
         })
     })
 
     app.post("/api/workouts", (req,res) => {
-        db.Workout.create({})
-        .then(Workout => {
-            res.json(Workout)
-        .catch(err => {
-                res.json(err)
-            })
+        Workout.create({})
+        .then(workout => {
+            res.json(workout)
+        }).catch(err => {
+            res.json(err)
         })
     })
 
-    app.post("/api/workouts/range", (req, res) => {
-        db.Workout.create({})
-        .then(Workout => res.json(Workout))
+    app.get("/api/workouts/range", (req, res) => {
+        Workout.find({}).limit(7)
+        .then(workout => res.json(workout))
         .catch(err => {
             res.json(err)
         })
@@ -41,11 +38,11 @@ module.exports = (app) => {
 
     app.put("/api/workouts/:id", (req, res) => {
         const newExercise = req.body
-        db.Workout.update( 
+        Workout.update( 
             { _id: req.params.id },
             { $push: {exercises: newExercise }},
         )
-        .then(Workout => res.json(Workout))
+        .then(workout => res.json(workout))
         .catch(err => {
             res.json(err)
         })
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,12 +2,12 @@ const path = require ("path");
 
 module.exports = function (app) {
     app.get("/exercise", (req, res) => {
-        res.sendFile(path.join(__dirname, "..public/exercise.html"));
+        res.sendFile(path.join(__dirname, "../public/exercise.html"));
     });
 
-    app.get("/", (req, res) => {
-        res.sendFile(path.join(__dirname, "../public/index.html"));
-    });
+    // app.get("/", (req, res) => {
+    //     res.sendFile(path.join(__dirname, "../public/index.html"));
+    // });
 
     app.get("/stats", (req, res) => {
         res.sendFile(path.join(__dirname, "../public/stats.html"))

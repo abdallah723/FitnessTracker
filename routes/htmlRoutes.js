5469b913340b6ab45edc9ce23da41fbf3d16ce82
abdallah723 <ibrham11@gmail.com>
2020-06-02 17:39:02 -0400

    fixed routing issue

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -5,9 +5,9 @@ module.exports = function (app) {
         res.sendFile(path.join(__dirname, "../public/exercise.html"));
     });
 
-    // app.get("/", (req, res) => {
-    //     res.sendFile(path.join(__dirname, "../public/index.html"));
-    // });
+    app.get("/", (req, res) => {
+        res.sendFile(path.join(__dirname, "../public/index.html"));
+    });
 
     app.get("/stats", (req, res) => {
         res.sendFile(path.join(__dirname, "../public/stats.html"))
